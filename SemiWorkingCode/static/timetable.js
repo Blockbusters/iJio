4f1af056c86f9c439bea07b9f143ae35f4794141
Nicholas <nlzz_22@hotmail.com>
2015-06-27 19:47:39 +0800

    Create timetable.js

---
+++ SemiWorkingCode/static/timetable.js
@@ -0,0 +1,7 @@
+var main = function(){
+    $('.subheader').on('click', function(){
+        alert('AM = Morning, NN = Afternoon, EV = Evening, PM = Night');
+    });
+};
+
+$(document).ready(main);
